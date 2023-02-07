85b50cda321b0256274fb8615ea62df5e1f6e980
ruggeropaolobasile <ruggeropaolobasile@gmail.com>
2023-02-07 17:03:32 +0100

    End of section 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,21 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
+
+        public Double Length { get; set; }
+
+        public Guid RegionId { get; set; }
+
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation Property
+        public Region Region { get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+
+        public Guid Id { get; set; }
+
+        public String Code { get; set; }
+
+    }
+}
